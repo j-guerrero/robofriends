8aad319ff9e3078e892fe51f555cc1896fea359b
Josh Guerrero <jrg99654@gmail.com>
2019-06-22 15:48:36 -0800

    Updates

---
+++ precache-manifest.e27c645d71faae5b6e046f07c13b6d09.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a618036502061ba88f8984fee515132f",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "8d8ad546c8bba8b27b11",
+    "url": "/robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "bfc85e10c22f36354e64",
+    "url": "/robofriends/static/css/main.8123de51.chunk.css"
+  },
+  {
+    "revision": "8d8ad546c8bba8b27b11",
+    "url": "/robofriends/static/js/2.c05ccf0c.chunk.js"
+  },
+  {
+    "revision": "bfc85e10c22f36354e64",
+    "url": "/robofriends/static/js/main.0d707a91.chunk.js"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
