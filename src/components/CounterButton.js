c4b4a3cdd495b48b4d3defcbc404105ab8ef595d
Josh Guerrero <jrg99654@gmail.com>
2019-06-22 15:14:07 -0800

    reupload w/ CounterButton

---
+++ src/components/CounterButton.js
@@ -0,0 +1,38 @@
+import React, { PureComponent } from 'react';
+
+
+// Wil only render when 'props' are changed
+class CounterButton extends PureComponent {
+
+	constructor(){
+		super();
+		this.state = {
+			count: 0
+		}
+	}
+
+	shouldComponentUpdate(nextProps, nextState){
+		if (this.state.count !== nextState.count){
+			return true
+		}
+
+		return false;
+	}
+
+	updateCount = () => {
+		this.setState(state =>{
+			return {count: state.count + 1}
+		})
+	}
+	
+	render(){
+		console.log('CounterButton');
+		return (
+			<button color={this.props.color} onClick={this.updateCount}>
+				Count: {this.state.count}
+			</button>
+		);
+	}
+}
+
+export default CounterButton;
---
+++ src/containers/App.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { setSearchField, requestRobots } from '../actions.js';
+
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox.js';
+import Scroll from '../components/Scroll';
+import ErrorBoundry from '../components/ErrorBoundry.js';
+import Header from '../components/Header.js';
+
+import './App.css';
+
+
+const mapStateToProps = (state) =>{
+	return {
+		searchField: state.searchRobots.searchField,
+		robots: state.requestRobots.robots,
+		isPending: state.requestRobots.isPending,
+		error: state.requestRobots.error
+	}
+}
+
+const mapDispatchToProps = (dispatch) => {
+	return {
+		onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+		onRequestRobots: () => dispatch(requestRobots())
+	}
+}
+
+class App extends Component {
+
+	componentDidMount(){
+		this.props.onRequestRobots();
+	}
+
+	render(){
+		const { searchField, onSearchChange, robots, isPending } = this.props;
+		const filteredRobots = robots.filter(robot =>{
+		return robot.name.toLowerCase().includes(searchField.toLowerCase());
+		})
+		return (
+			<div className ='tc'>
+				<Header />
+				<SearchBox searchChange={onSearchChange}/>
+				<Scroll>
+				{ isPending ? <h1> Loading </h1> :
+					<ErrorBoundry>
+							<CardList robots={filteredRobots}/>
+						</ErrorBoundry>
+				}
+				</Scroll>
+			</div>
+			);
+		}
+
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
